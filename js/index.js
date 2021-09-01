bbcc777274f65604a6b84b01691fed56207ac487
C2thehris <chriskreat@hotmail.com>
2021-09-01 18:26:48 -0400

    Split page into multiple section with js

---
+++ js/index.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+    present('academic')
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+        button.onclick = function () {
+            console.log(button.dataset.div)
+            present(button.dataset.div)
+        }
+    })
+})
+
+function present(div_name) {
+    clearAll()
+    document.querySelector(`#${div_name}`).style.display = 'block';
+}
+
+function clearAll() {
+    const divs = document.querySelectorAll('body>div')
+    divs.forEach(div => {
+        div.style.display = 'none';
+    })
+}
